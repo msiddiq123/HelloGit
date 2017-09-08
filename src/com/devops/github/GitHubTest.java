df59345be7885384d1aba6f20ed8c9ded9ecc27f
Maroof <maroof.siddique2013@gmail.com>
2017-09-08 19:10:38 +0530

    First commit for HelloGit

---
+++ src/com/devops/github/GitHubTest.java
@@ -0,0 +1,12 @@
+package com.devops.github;
+
+public class GitHubTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("------Git Hub Test-1-------on------8-SEP-2017");
+
+	}
+
+}
